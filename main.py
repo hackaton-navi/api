9b46a4200eaadbd1e2622671d4b849622596215a
viniciusbaca <viniciusfbaca@usp.br>
2021-09-25 20:23:39 -0300

    trying fix heroku

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 CORS(app)
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=os.environ.get("PORT", 33507))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 @app.route("/")
 def hello_world():
