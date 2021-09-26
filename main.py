98ab2ba4d2891e8b5c27c217abc1eea5e9b97d6c
Enrico <enricorobazzi@gmail.com>
2021-09-25 23:38:15 -0300

    Includes performance

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, request
 from flask_cors import CORS
 import pandas as pd
 import os
+from services.yfinance import get_stocks_prices
 
 app = Flask(__name__)
 CORS(app)
@@ -87,6 +88,91 @@ def stocks():
 	pivoted_df.reset_index(inplace=True)
 
 	return Response(pivoted_df.to_json(orient="records"), mimetype='application/json')
+
+@app.route("/ranges-ebitda", methods=["post"])
+def ranges_ebitda():
+	data = request.get_json()
+	value0 = int(data["value0"])
+	value1 = int(data["value1"])
+	value2 = int(data["value2"])
+	value3 = int(data["value3"])
+
+	ebitda_df = get_ebitda_df()
+	df = merged_esg_scores()
+	df = df.loc[(df["parent_aspect"] == "S&P Global ESG Score") | (df["aspect"] == "S&P Global ESG Score")]
+	pivoted_df = df.pivot_table(index='ticker', columns='aspect', values='score_value')
+	pivoted_df = pivoted_df.merge(ebitda_df, how="inner", on="ticker")
+
+	columns = pivoted_df.columns
+	for i in range(4, len(columns)-1):
+		prev_column = columns[i+1]
+		column = columns[i]
+		pivoted_df[column] = (pivoted_df[column]/pivoted_df[prev_column]) - 1
+	
+	pivoted_df = pivoted_df.drop(columns=pivoted_df.columns[-1])
+	pivoted_df = pivoted_df.rename(columns={"Environmental Dimension": "E", "Governance & Economic Dimension": "G", "Social Dimension": "S", "S&P Global ESG Score": "score"})
+
+	high_score = pivoted_df[(pivoted_df["score"] < value3) & (pivoted_df["score"] > value2)]
+	medium_score = pivoted_df[(pivoted_df["score"] < value2) & (pivoted_df["score"] > value1)]
+	low_score = pivoted_df[(pivoted_df["score"] < value1) & (pivoted_df["score"] > value0)]
+
+	high_grouped = high_score.mean().to_frame().T
+	medium_grouped = medium_score.mean().to_frame().T
+	low_grouped = low_score.mean().to_frame().T
+
+	high_grouped["portfolio"] = "High Score ESG"
+	medium_grouped["portfolio"] = "Medium Score ESG"
+	low_grouped["portfolio"] = "Low Score ESG"
+
+	all_portfolios = pd.concat([high_grouped, medium_grouped, low_grouped])
+
+	return Response(all_portfolios.to_json(orient="records"), mimetype='application/json')
+
+@app.route("/ranges-stock-price", methods=["post"])
+def ranges_stock_price():
+	data = request.get_json()
+	value0 = int(data["value0"])
+	value1 = int(data["value1"])
+	value2 = int(data["value2"])
+	value3 = int(data["value3"])
+
+	df = merged_esg_scores()
+	df = df.loc[(df["parent_aspect"] == "S&P Global ESG Score") | (df["aspect"] == "S&P Global ESG Score")]
+	pivoted_df = df.pivot_table(index='ticker', columns='aspect', values='score_value')
+
+	prices = get_stocks_prices([stock + ".SA" for stock in pivoted_df.index.tolist()])
+	prices["DATE"] = pd.to_datetime(prices["DATE"]).dt.strftime('%Y-%m-%d')
+	prices.set_index("DATE", inplace=True)
+	prices = prices.astype('float').pct_change().iloc[1:]
+
+	for column in prices.columns:
+		prices[column] = (1 + prices[column]).cumprod() - 1 
+
+	prices = prices.T
+
+	prices = prices.reset_index().rename(columns={"index": "ticker"})
+	prices["ticker"] = [ticker[:-3] for ticker in prices["ticker"]]
+	prices.set_index("ticker")
+	
+	pivoted_df = pivoted_df.rename(columns={"Environmental Dimension": "E", "Governance & Economic Dimension": "G", "Social Dimension": "S", "S&P Global ESG Score": "score"})
+	pivoted_df = pivoted_df.merge(prices, how="inner", on="ticker")
+
+	high_score = pivoted_df[(pivoted_df["score"] < value3) & (pivoted_df["score"] > value2)]
+	medium_score = pivoted_df[(pivoted_df["score"] < value2) & (pivoted_df["score"] > value1)]
+	low_score = pivoted_df[(pivoted_df["score"] < value1) & (pivoted_df["score"] > value0)]
+
+	high_grouped = high_score.mean().to_frame().T
+	medium_grouped = medium_score.mean().to_frame().T
+	low_grouped = low_score.mean().to_frame().T
+
+	high_grouped["portfolio"] = "High Score ESG"
+	medium_grouped["portfolio"] = "Medium Score ESG"
+	low_grouped["portfolio"] = "Low Score ESG"
+
+	all_portfolios = pd.concat([high_grouped, medium_grouped, low_grouped])
+
+	return Response(all_portfolios.to_json(orient="records"), mimetype='application/json')
+
 
 if __name__ == "__main__":
 	app.run(host=os.environ.get("HOST", "127.0.0.1"), port=os.environ.get("PORT", 5000))
---
+++ services/yfinance.py
@@ -0,0 +1,27 @@
+import urllib.request
+import json
+import time
+from datetime import date, timedelta, datetime
+import csv
+import pandas as pd
+import numpy as np
+
+def get_price(code):
+	timestamp_beginning = time.mktime((date.today()-timedelta(days=365)).timetuple())
+	url = "https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%s&period2=%s&interval=1d&events=history&includeAdjustedClose=true"%(code, int(timestamp_beginning), int(time.time()))
+	response = urllib.request.urlopen(url)
+
+	prices = [{"date": datetime.date(datetime.strptime(x[0], "%Y-%m-%d")), "value": x[4]} for x in list(csv.reader(response.read().decode().splitlines(), delimiter=','))[1:]]
+
+	df = pd.DataFrame([[p["date"], p["value"] if p["value"] != "null" else np.nan] for p in prices], columns=['DATE', code])
+	return df
+
+def get_stocks_prices(stocks):
+	stocks_prices = None
+	for stock in stocks:
+		df = get_price(stock)
+		if stocks_prices is None:
+			stocks_prices = df
+		else:
+			stocks_prices = stocks_prices.merge(df, how="inner", on="DATE")
+	return stocks_prices
